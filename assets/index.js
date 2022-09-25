6ea9e9b22dfef84ab6284d4c39cec8706834fb94
Ali Tunç <tunchalikali@gmail.com>
2022-09-26 00:51:16 +0300

    export the assets

---
+++ assets/index.js
@@ -0,0 +1,11 @@
+const assets = {
+  icons: {
+    searchIcon: require("./icons/ic-search.svg"),
+    hamburgerOpen: require("./icons/open-menu.png"),
+  },
+  logos: {
+    whiteLogo: require("./logos/hurriyet-logo-white.svg"),
+  },
+};
+
+export default assets;
