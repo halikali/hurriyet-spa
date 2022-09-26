8da0eb6f130279c36643a2847bd7648944e6a444
Ali Tunç <tunchalikali@gmail.com>
2022-09-27 00:44:46 +0300

    complete the hot agenda component staticly

---
+++ components/pages/Home/HotAgenda.tsx
@@ -0,0 +1,33 @@
+import Image from "next/image";
+import { FC } from "react";
+
+import HotAgendNews from "../../../mocks/HotAgenda.json";
+import style from "../../../styles/components/pages/Home/HotAgenda.module.css";
+
+const HotAgenda: FC = () => {
+  const { news } = HotAgendNews;
+
+  return (
+    <div className={style.container}>
+      <h2 className={style.title}>
+        <a href="https://www.hurriyet.com.tr/gundem/" target={"_blank"}>Sıcak Gündem</a>
+      </h2>
+      <div className={style.wrapper}>
+        {news.map((item) => (
+          <a href={item.href} key={item.href} target={"_blank"} className={style.imageWrapper}>
+            <Image
+              src={item.imageUrl}
+              alt={item.title}
+              title={item.title}
+              width={800}
+              height={550}
+              className={style.image}
+            />
+          </a>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default HotAgenda;
