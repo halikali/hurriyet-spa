2c58947b42c88620db102ffda05e1de21a72766b
Ali Tunç <tunchalikali@gmail.com>
2022-09-27 00:43:58 +0300

    set domains for next image

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    domains: ["i4.hurimg.com"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
