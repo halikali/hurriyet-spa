3f7bacefdcfddae6198b97e8cfd8abeddbd1d2d9
Ali Tunç <tunchalikali@gmail.com>
2022-09-28 16:01:45 +0300

    complete the home carousel staticly

---
+++ components/pages/Home/HomeCarousel.tsx
@@ -0,0 +1,39 @@
+import Image from "next/image";
+import { FC } from "react";
+import { Navigation, Pagination } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import HomeCarouselNews from "mocks/HomeCarousel.json";
+
+const HomeCarousel: FC = () => {
+  const { news } = HomeCarouselNews;
+
+  return (
+    <div>
+      <Swiper
+        navigation
+        pagination={{ clickable: true }}
+        modules={[Pagination, Navigation]}
+        className="home-carousel"
+        loop={true}
+      >
+        {news?.map((item, i) => (
+          <SwiperSlide>
+            <a href={item.href} key={`${item.href}${i}`}>
+              <Image
+                src={item.src}
+                alt={item.title}
+                title={item.title}
+                width={600}
+                height={400}
+                layout="responsive"
+              />
+            </a>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
+};
+
+export default HomeCarousel;
