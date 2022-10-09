85f0a675198d6d34a2237325014f8babb6a35394
Ali Tunç <tunchalikali@gmail.com>
2022-10-09 17:56:17 +0300

    link to mock routes

--- components/shared/NewsCard/HorizontalNewsCardWithSpotText.tsx
+++ components/shared/NewsCard/HorizontalNewsCardWithSpotText.tsx
@@ -1,48 +1,51 @@
 import Image from "next/image";
+import Link from "next/link";
 import { FC } from "react";
 
 import style from "styles/components/shared/NewsCard/HorizontalNewsCardWithSpotText.module.css";
 
 const HorizontalNewsCardWithSpotText: FC = () => {
   return (
-    <figure className={style.newsCard}>
-      <div className={style.imageWrapper}>
-        <Image
-          src={
-            "https://i4.hurimg.com/i/hurriyet/75/424x282/63359c554e3fe113fc195012.jpg"
-          }
-          alt={"Tek gerçek kraliçenin kararı oğlunu çok kızdırdı"}
-          title={"Tek gerçek kraliçenin kararı oğlunu çok kızdırdı"}
-          width={160}
-          height={90}
-          layout={"responsive"}
-          className={style.image}
-        />
-      </div>
-      <figcaption className={style.figcaption}>
-        <a
-          href="https://www.hurriyet.com.tr/arama/#/?page=1&key=danimarka-kralicesi-margrethe-2&how=Article,Column,NewsPhotoGallery,NewsVideo,Recipe&isDetail=false"
-          title="Tek gerçek kraliçe'nin kararı oğlunu çok kızdırdı"
-          className={style.title}
-        >
-          {"'Tek gerçek kraliçe'nin kararı oğlunu çok kızdırdı"}
-        </a>
-        <a
-          href="https://www.hurriyet.com.tr/arama/#/?page=1&key=danimarka-kralicesi-margrethe-2&how=Article,Column,NewsPhotoGallery,NewsVideo,Recipe&isDetail=false"
-          title="Tek gerçek kraliçe'nin kararı oğlunu çok kızdırdı"
-          className={style.spotText}
-        >
-          {"'Babaanneleri unvanlarını alıp çocuklarıma zarar verdi'"}
-        </a>
-        <a
-          href="https://www.hurriyet.com.tr/arama/#/?page=1&key=danimarka-kralicesi-margrethe-2&how=Article,Column,NewsPhotoGallery,NewsVideo,Recipe&isDetail=false"
-          title="#danimarka kraliçesi margrethe 2"
-          className={style.tag}
-        >
-          #danimarka kraliçesi margrethe 2
-        </a>
-      </figcaption>
-    </figure>
+    <Link href={"/gundem/galeri/h"}>
+      <figure className={style.newsCard}>
+        <div className={style.imageWrapper}>
+          <Image
+            src={
+              "https://i4.hurimg.com/i/hurriyet/75/424x282/63359c554e3fe113fc195012.jpg"
+            }
+            alt={"Tek gerçek kraliçenin kararı oğlunu çok kızdırdı"}
+            title={"Tek gerçek kraliçenin kararı oğlunu çok kızdırdı"}
+            width={160}
+            height={90}
+            layout={"responsive"}
+            className={style.image}
+          />
+        </div>
+        <figcaption className={style.figcaption}>
+          <a
+            href="https://www.hurriyet.com.tr/arama/#/?page=1&key=danimarka-kralicesi-margrethe-2&how=Article,Column,NewsPhotoGallery,NewsVideo,Recipe&isDetail=false"
+            title="Tek gerçek kraliçe'nin kararı oğlunu çok kızdırdı"
+            className={style.title}
+          >
+            {"'Tek gerçek kraliçe'nin kararı oğlunu çok kızdırdı"}
+          </a>
+          <a
+            href="https://www.hurriyet.com.tr/arama/#/?page=1&key=danimarka-kralicesi-margrethe-2&how=Article,Column,NewsPhotoGallery,NewsVideo,Recipe&isDetail=false"
+            title="Tek gerçek kraliçe'nin kararı oğlunu çok kızdırdı"
+            className={style.spotText}
+          >
+            {"'Babaanneleri unvanlarını alıp çocuklarıma zarar verdi'"}
+          </a>
+          <a
+            href="https://www.hurriyet.com.tr/arama/#/?page=1&key=danimarka-kralicesi-margrethe-2&how=Article,Column,NewsPhotoGallery,NewsVideo,Recipe&isDetail=false"
+            title="#danimarka kraliçesi margrethe 2"
+            className={style.tag}
+          >
+            #danimarka kraliçesi margrethe 2
+          </a>
+        </figcaption>
+      </figure>
+    </Link>
   );
 };
 
