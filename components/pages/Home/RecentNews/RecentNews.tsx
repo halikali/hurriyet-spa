0c8adce66de5834af1fd730cd82014b4d4919f0a
Ali Tunç <tunchalikali@gmail.com>
2022-10-09 17:53:48 +0300

    edit recent news component

--- components/pages/Home/RecentNews/RecentNews.tsx
+++ components/pages/Home/RecentNews/RecentNews.tsx
@@ -1,6 +1,8 @@
 import { FC } from "react";
 import RecentNewsListItem from "./RecentNewsListItem";
+import Image from "next/image";
 
+import assets from "assets";
 import style from "styles/components/pages/Home/RecentNews.module.css";
 
 const RecentNews: FC = () => {
@@ -8,7 +10,14 @@ const RecentNews: FC = () => {
     <div className={style.recentNews}>
       <div className={style.header}>
         <h3 className={style.title}>güncel haberler</h3>
-        <span>{"->"}</span>
+        <Image
+          src={assets.icons.rightArrow.default.src}
+          width={15}
+          height={15}
+          alt={"right arrow icon"}
+          title={"right arrow icon"}
+          className={style.arrow}
+        />
       </div>
 
       <ul className={style.list}>
